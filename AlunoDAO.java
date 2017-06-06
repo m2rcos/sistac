0d6809cdbd9032f65fa8e592ed6698612b289ea4
Ednelson <ednelsonmatheus659@gmail.com>
2017-06-06 19:28:44 -0300

    classeDao - SISTAC

---
+++ AlunoDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import java.util.List;
+
+import modelo.Aluno;
+
+public interface AlunoDAO {
+	
+	void salvar(Aluno aluno) throws Exception;
+	void deletar(Aluno aluno) throws Exception;
+	Aluno buscarAlunoID(long id) throws Exception;
+	Aluno buscarAlunoMatricula(String matricula) throws Exception;
+	Aluno buscarAlunoCPF(String cpf) throws Exception;
+	List<Aluno> listar() throws Exception;
+	boolean verificaExistencia(long id,String nomeTabela,String campo) throws Exception;
+	List<Aluno> buscaOrientadorAluno(long id)throws Exception;
+}
